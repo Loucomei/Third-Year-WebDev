216cfba6bbd4492177aebaa6746ff4df6fffa4e4
Ronnie <afn22ehu@uea.ac.uk>
2024-11-10 19:36:44 +0000

    tidy up

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,11 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { logoutUser } from "../features/appUsers/appUserSlice";
 
 const NavBar = () => {
-
-  const user = useSelector((state) => state.appUsers.user)
-
-  //console.log(user)
-
+  const user = useSelector((state) => state.appUsers.user);
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
@@ -16,96 +12,121 @@ const NavBar = () => {
     console.log("logout");
     dispatch(logoutUser());
     navigate("/");
-  }
+  };
 
   const login = () => {
     console.log("login");
-    navigate("/login")
-  }
+    navigate("/login");
+  };
 
   const register = () => {
     console.log("login");
-    navigate("/register")
-  }
+    navigate("/register");
+  };
 
-    return (
-        <div className="navbar bg-base-100" >
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor">
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M4 6h16M4 12h8m-8 6h16" />
-        </svg>
-      </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        <li><NavLink to="/">Home </NavLink></li>
-        <li>
-          <a>Account</a>
-          <ul className="p-2">
-          {
-          user ? (
-            <>
-              <li><NavLink to="/settings">Settings </NavLink></li>
-              <li><button onClick={logout}>Sign Out</button></li>
-            </>
-          ):(
-            <>
-              <li><NavLink to="/login">Login </NavLink></li>
-              <li><NavLink to="/register">Register </NavLink></li>
-            </>
-          )
-        }
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <NavLink to="/">Home </NavLink>
+            </li>
+            <li>
+              <a>Account</a>
+              <ul className="p-2">
+                {user ? (
+                  <>
+                    <li>
+                      <NavLink to="/settings">Settings </NavLink>
+                    </li>
+                    <li>
+                      <button onClick={logout}>Sign Out</button>
+                    </li>
+                  </>
+                ) : (
+                  <>
+                    <li>
+                      <NavLink to="/login">Login </NavLink>
+                    </li>
+                    <li>
+                      <NavLink to="/register">Register </NavLink>
+                    </li>
+                  </>
+                )}
+              </ul>
+            </li>
           </ul>
-        </li>
-      </ul>
-    </div>
-    <button className="btn btn-ghost text-xl"><NavLink to="/">FinalOffer</NavLink></button>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><NavLink to="/">Home </NavLink></li>
-      {
-          user ? (
+        </div>
+        <button className="btn btn-ghost text-xl">
+          <NavLink to="/">FinalOffer</NavLink>
+        </button>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <NavLink to="/">Home </NavLink>
+          </li>
+          {user ? (
             <ul>
-              <li><NavLink to="/settings">Settings </NavLink></li>
-              <li><button onClick={logout}>Sign Out</button></li>
+              <li>
+                <NavLink to="/settings">Settings </NavLink>
+              </li>
+              <li>
+                <button onClick={logout}>Sign Out</button>
+              </li>
             </ul>
-          ):(
+          ) : (
             <>
-              <li><NavLink to="/login">Login </NavLink></li>
-              <li><NavLink to="/register">Register </NavLink></li>
+              <li>
+                <NavLink to="/login">Login </NavLink>
+              </li>
+              <li>
+                <NavLink to="/register">Register </NavLink>
+              </li>
             </>
-          )
-        }
-    </ul>
-  </div>
-  <div className="navbar-end">
-  <input
-  type="checkbox"
-  value="dark"
-  className="toggle theme-controller col-span-2 col-start-1 row-start-1 border-sky-400 bg-amber-300 [--tglbg:theme(colors.sky.500)] checked:border-blue-800 checked:bg-blue-300 checked:[--tglbg:theme(colors.blue.900)]" />
-    {
-      (useLocation().pathname == '/register' || useLocation().pathname == '/login') ? (
-        <>
-        </>
-      ) : (
-        <UserContainer user={user} logout={logout} login={login} register={register} />
-      )
-    }
-  </div>
-</div>
-    )
-}
+          )}
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <input
+          type="checkbox"
+          value="dark"
+          className="toggle theme-controller col-span-2 col-start-1 row-start-1 border-sky-400 bg-amber-300 [--tglbg:theme(colors.sky.500)] checked:border-blue-800 checked:bg-blue-300 checked:[--tglbg:theme(colors.blue.900)]"
+        />
+        {useLocation().pathname == "/register" ||
+        useLocation().pathname == "/login" ? (
+          <></>
+        ) : (
+          <UserContainer
+            user={user}
+            logout={logout}
+            login={login}
+            register={register}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default NavBar
+export default NavBar;
--- src/components/UserContainer.jsx
+++ src/components/UserContainer.jsx
@@ -1,25 +1,20 @@
 import { Link } from "react-router-dom";
-const UserContainer = ( props ) => {
-    const { user, login, logout, register } = props
-    return (
-        <ul className="logins">
-        {
-            user ? (
-                <li>
-                    {"Hi "}
-                    {user?.username?.toUpperCase()}
-                    <div className="avatar">
-                        <div className="w-8 rounded-full">
-                            <img src={user?.picture} />
-                        </div>
-                    </div>
-                </li>
-            ) : (
-                <>
-                </>
-            )
-        }
-        </ul>
-    )
-}
+const UserContainer = (props) => {
+  const { user, login, logout, register } = props;
+  return (
+    <ul className="logins text-accent">
+      {user ? (
+        <li>
+          {"Hi "}
+          {user?.username?.toUpperCase()}
+          <div className="avatar">
+            <div className="w-8 rounded-full"></div>
+          </div>
+        </li>
+      ) : (
+        <></>
+      )}
+    </ul>
+  );
+};
 export default UserContainer;
